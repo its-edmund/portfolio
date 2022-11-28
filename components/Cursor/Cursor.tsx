2dd4d16593e082a19f10665b8ca74702d0bc382b
Edmund Xin <marxin@tesla.com>
2022-11-28 13:19:44 -0800

    Initial commit

---
+++ components/Cursor/Cursor.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import useMousePosition from "../../hooks/useMousePosition";
+import _ from "lodash";
+
+const Cursor = () => {
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
+  const { x, y } = useMousePosition();
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) =>
+      setTimeout(() => {
+        setCoord({ x: e.clientX, y: e.clientY });
+      }, 50)
+    );
+
+    return () => {
+      document.removeEventListener("mousemove", (e) =>
+        setTimeout(() => {
+          setCoord({ x: e.clientX, y: e.clientY });
+        }, 50)
+      );
+    };
+  }, []);
+
+  return (
+    <div
+      className="w-8 h-8 bg-white rounded-full fixed mix-blend-difference"
+      style={{
+        left: `${coord.x}px`,
+        top: `${coord.y}px`,
+        transform: "translate(-50%, -50%)",
+      }}
+    ></div>
+  );
+};
+
+export default Cursor;
